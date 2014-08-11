86c72ffff890f02b582eec4802178b517b2c7370
Kit Brown-Watts <kbwatts@gmail.com>
2014-08-11 13:33:07 +1000

    Updated css location

--- New/360v2sidebar.js
+++ New/360v2sidebar.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var cssPath = '//www.library.uq.edu.au/360Link/Testing/360/',libDetailPageId, newHref, libCurrentURL, record, recordParts, thisID,libCurrentURL = window.location.hash.substring(1);
+  var cssPath = '//www.library.uq.edu.au/360Link/New/',libDetailPageId, newHref, libCurrentURL, record, recordParts, thisID,libCurrentURL = window.location.hash.substring(1);
   // Add custom styles
   $('head').append('<link rel="stylesheet" type="text/css" href="' + cssPath + '360v2sidebar.css" />');
 
