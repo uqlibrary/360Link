7348d98d2b5d6a271b6b85d84fa82158cec98b2d
Kit Brown-Watts <kbwatts@gmail.com>
2014-08-11 15:11:47 +1000

    Using event.stopPropogation()

--- New/360v2sidebar.js
+++ New/360v2sidebar.js
@@ -1,12 +1,14 @@
 document.write('<link rel="stylesheet" type="text/css" href="https://www.library.uq.edu.au/360Link/New/360v2sidebar.css">');
 
-function validateForm() {
+function validateForm(event, element) {
 	if (document.getElementById("reportProblemEmail").value == "") {
 		alert("Please enter an email address before sending your report.");
+		event.stopPropagation();
+		event.cancelBubble = true;
 	}
 }
 
 
 
 
-document.getElementsByClassName("report-problem-form hide")[0].innerHTML = "<div class='report-problem-form hide'><p>Send report and we'll investigate the problem.Enter your name and email only if you wish to be contacted.</p><label for='reportProblemName'>Name:</label><input type='text' id='reportProblemName' /><label for='reportProblemEmail' name='email'>Email:</label><input type='text' id='reportProblemEmail' /><label for='reportProblemMessage'>Message:</label><textarea rows='3' id='reportProblemMessage'></textarea><button class='btn report-problem-cancel'>Cancel</button><button class='btn btn-inverse report-problem-send' onClick='validateForm()'>Send Report</button></div>";
+document.getElementsByClassName("report-problem-form hide")[0].innerHTML = "<div class='report-problem-form hide'><p>Send report and we'll investigate the problem.Enter your name and email only if you wish to be contacted.</p><label for='reportProblemName'>Name:</label><input type='text' id='reportProblemName' /><label for='reportProblemEmail' name='email'>Email:</label><input type='text' id='reportProblemEmail' /><label for='reportProblemMessage'>Message:</label><textarea rows='3' id='reportProblemMessage'></textarea><button class='btn report-problem-cancel'>Cancel</button><button class='btn btn-inverse report-problem-send' onClick='validateForm(event,this)'>Send Report</button></div>";
