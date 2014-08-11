bd4f6bff1da86a58328237aa17fe1f81f77f9198
Kit Brown-Watts <kbwatts@gmail.com>
2014-08-11 15:34:47 +1000

    Simplified validate code

--- New/360v2sidebar.js
+++ New/360v2sidebar.js
@@ -1,7 +1,7 @@
 document.write('<link rel="stylesheet" type="text/css" href="https://www.library.uq.edu.au/360Link/New/360v2sidebar.css">');
 
 function validate() {
-	document.getElementsByClassName("report-problem-form hide")[0].innerHTML = "<div class='report-problem-form hide'><p>Send report and we'll investigate the problem.Enter your name and email only if you wish to be contacted.</p><label for='reportProblemName'>Name:</label><input type='text' onchange='validate()' id='reportProblemName' /><label for='reportProblemEmail' name='email'>Email:</label><input type='text' onchange='validate()' id='reportProblemEmail' /><label for='reportProblemMessage'>Message:</label><textarea rows='3' id='reportProblemMessage'></textarea><button class='btn report-problem-cancel'>Cancel</button><button class='btn btn-inverse report-problem-send' disabled='enabled'>Send Report</button></div>";
+	document.getElementsByClassName("btn btn-inverse report-problem-send")[0].disabled = false;
 }
 
 
