84325b380d2391445767a10b7626e7465dfb769c
Kit Brown-Watts <kbwatts@gmail.com>
2014-08-11 15:02:31 +1000

    Updated form validation script

--- New/360v2sidebar.js
+++ New/360v2sidebar.js
@@ -1,12 +1,22 @@
 document.write('<link rel="stylesheet" type="text/css" href="https://www.library.uq.edu.au/360Link/New/360v2sidebar.css">');
 
-function validateForm() {
-	if (document.getElementById("reportProblemEmail").value == "") {
-		alert("Please enter an email address before sending your report.")
-	}
+
+document.getElementsByClassName("report-problem-form hide")[0].innerHTML = "<div class='report-problem-form hide'><p>Send report and we'll investigate the problem.Enter your name and email only if you wish to be contacted.</p><label for='reportProblemName'>Name:</label><input type='text' id='reportProblemName' /><label for='reportProblemEmail' name='email'>Email:</label><input type='text' id='reportProblemEmail' /><label for='reportProblemMessage'>Message:</label><textarea rows='3' id='reportProblemMessage'></textarea><button class='btn report-problem-cancel'>Cancel</button><button class='btn btn-inverse new-report-problem-send' onClick='validateForm()'>Send Report</button></div>";
+
+
+
+
+jQuery(function($){
+	$('.new-report-problem-send').on('click', function(event) {	
+		if (document.getElementById("reportProblemEmail").value == "") {
+		alert("Please enter an email address before sending your report.");
+	} else {
+	$("#patronName").val($("#reportProblemName").val());
+	$("#patronEmail").val($("#reportProblemEmail").val());
+	$("#patronMessage").val($("#reportProblemMessage").val());
+	reportBadLink("Link Not Working");
+	$('.report-problem-form').hide();
+	showReportProblemConfirmation();
 }
-
-
-
-
-document.getElementsByClassName("report-problem-form hide")[0].innerHTML = "<div class='report-problem-form hide'><p>Send report and we'll investigate the problem.Enter your name and email only if you wish to be contacted.</p><label for='reportProblemName'>Name:</label><input type='text' id='reportProblemName' /><label for='reportProblemEmail' name='email'>Email:</label><input type='text' id='reportProblemEmail' /><label for='reportProblemMessage'>Message:</label><textarea rows='3' id='reportProblemMessage'></textarea><button class='btn report-problem-cancel'>Cancel</button><button class='btn btn-inverse report-problem-send' onClick='validateForm()'>Send Report</button></div>";
+});
+})
