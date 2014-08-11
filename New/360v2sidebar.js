2b101e30d13fc0a743ab06fe59ce39eaa0e1ac57
Kit Brown-Watts <kbwatts@gmail.com>
2014-08-11 13:28:41 +1000

    Added 360v2sidebar updates

---
+++ New/360v2sidebar.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+  var cssPath = '//www.library.uq.edu.au/360Link/Testing/360/',libDetailPageId, newHref, libCurrentURL, record, recordParts, thisID,libCurrentURL = window.location.hash.substring(1);
+  // Add custom styles
+  $('head').append('<link rel="stylesheet" type="text/css" href="' + cssPath + '360v2sidebar.css" />');
+
+});
